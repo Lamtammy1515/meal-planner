1b2ea43d85fe51c366306b04dbd58d997ff9f58b
Tammy Lam <lamtammy1515@gmail.com>
2020-12-07 23:18:50 -0600

    Added post request. able to create new food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,19 @@
 class FoodsController < ApplicationController
     def index 
-        render :json => Food.all, :include => :meal
+        render :json => Food.all, :include => :meal, :statis => 200
+    end
+
+    def create
+        meal = Meal.find_by(meal: params[:meal].downcase) 
+        food = Food.create(entree: params[:entree], meal: meal)
+        render :json => food, :include => :meal, :status => 201
     end 
+
+    def destroy 
+        food = Food.find(params[:id])
+        #is this the correct status code? 
+        render :json => {id: params[:id], message: "Record was successfully deleted"}, :status => 200
+        #tap into sqlit3 exceptions that are raised and create a custom error message
+   end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :meals, only: :index
-  resources :foods, only: :index
+  resources :foods, only: [:index, :destroy, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
